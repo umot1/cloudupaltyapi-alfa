5b8648f3f094c58c26953af83a1bf7cb33369f00
Glitch (enthusiastic-subdued-color) <none>
2020-11-24 12:58:20 +0000

    🐩👫 Checkpoint ./main.js:8706422/2355 ./komutlar/black.js:8706422/3572

--- main.js
+++ main.js
@@ -28,48 +28,6 @@ fs.readdir("./komutlar/", (err, files) => {
     });
   });
 });
-const { Command } = require('discord.js-commando');
-
-module.exports = class BlacklistUserCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'blacklist-user',
-			aliases: ['blacklist'],
-			group: 'admin',
-			memberName: 'blacklist-user',
-			description: 'Kullanıcıyı bottan banlamanızı sağlar.',
-			throttling: {
-				usages: 2,
-				duration: 3
-			},
-
-			args: [
-				{
-					key: 'user',
-					prompt: 'whom do you want to blacklist?\n',
-					type: 'user'
-				}
-			]
-		});
-	}
-
-	hasPermission(msg) {
-		return this.client.isOwner()"766693805663191061"));
-	}
-
-	run(msg, { user }) {
-		if (this.client.isOwner("766693805663191061")) return msg.reply(client.config.customEmojis.basarisiz + ' bot sahibi blackliste alınamaz.');
-
-		const blacklist = this.client.provider.get('global', 'userBlacklist', []);
-		if (blacklist.includes(user.id)) return msg.reply(client.config.customEmojis.basarisiz + ' bu kişi zaten blacklistde bulunuyor.');
-
-		blacklist.push(user.id);
-		this.client.provider.set('global', 'userBlacklist', blacklist);
-
-		return msg.reply(`${client.config.customEmojis.basarili} ${user.tag} bottan banlandı.`);
-	}
-};
-
 client.reload = command => {
   return new Promise((resolve, reject) => {
     try {
